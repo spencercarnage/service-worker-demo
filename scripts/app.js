cab099f11fac164c5d164fcabdc47907b1062b24
spencercarnage <spencercarnage@gmail.com>
2016-04-24 11:51:11 -0700

    fixed link to service worker script in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('scripts/service-worker.js')
     .then(function (registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     })

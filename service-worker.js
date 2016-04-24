b7cba60333d3fbec9cd5cce0e1440d251029456a
Spencer Carney <spencer.carney@crowdrise.com>
2016-04-24 14:34:18 -0700

    added index to sw cache

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,14 @@
-//var CACHE_NAME = 'my-site-cache-v1';
-//
-//var urlsToCache = [
-//    '/service-worker-demo/scripts/app.js',
-//    '/service-worker-demo/stylesheets/github-light.css'
-//];
-//
-console.log('sup');
-//
-//self.addEventListener('install', function (event) {
-//    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
-//        console.log('opened cache');
-//
-//        return cache.addAll(urlsToCache);
-//    }));
-//});
+var CACHE_NAME = 'my-site-cache-v1';
+
+var urlsToCache = [
+    '/'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+        console.log('opened cache');
+
+        return cache.addAll(urlsToCache);
+    }));
+});
 

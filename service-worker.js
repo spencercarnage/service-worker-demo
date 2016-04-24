45842e700506cfdbbdeabae179d00957a8225d05
Spencer Carney <spencer.carney@crowdrise.com>
2016-04-24 14:33:31 -0700

    commented out sw caching

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-var CACHE_NAME = 'my-site-cache-v1';
+//var CACHE_NAME = 'my-site-cache-v1';
+//
+//var urlsToCache = [
+//    '/service-worker-demo/scripts/app.js',
+//    '/service-worker-demo/stylesheets/github-light.css'
+//];
+//
+console.log('sup');
+//
+//self.addEventListener('install', function (event) {
+//    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+//        console.log('opened cache');
+//
+//        return cache.addAll(urlsToCache);
+//    }));
+//});
 
-var urlsToCache = [
-    '/service-worker-demo/scripts/app.js',
-    '/service-worker-demo/stylesheets/github-light.css'
-];
-
-console.log('here');
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
-        console.log('opened cache');
-
-        return cache.addAll(urlsToCache);
-    }));
-});

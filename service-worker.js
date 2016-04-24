59ac7321d1807ea58c21d443b91b5af039b3d505
Spencer Carney <spencer.carney@crowdrise.com>
2016-04-24 14:52:00 -0700

    updated sw cache name

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'site-cache';
 var VERSION = 'v1::';
 
 function updateStaticCache () {
@@ -21,7 +21,6 @@ self.addEventListener('activate', function (event) {
         // Remove caches whose name is no longer valid
         return Promise.all(
             keys.filter(function (key) {
-                console.log('filter', key);
                 return key.indexOf(VERSION) !== 0;
             }).map(function (key) {
                 return caches.delete(key);

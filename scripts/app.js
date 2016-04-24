f4e122b50c568c97aedd33792128fc5da9282ebf
Spencer Carney <spencer.carney@crowdrise.com>
2016-04-24 13:58:34 -0700

    updated path to sw js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('/service-worker-demo/service-worker.js')
         .then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
